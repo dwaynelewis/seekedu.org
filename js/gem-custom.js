b6dea13dc6de53fd6c95cea80403939329f309fe
jbtristan <54360735+jbtristan@users.noreply.github.com>
2020-02-01 12:31:18 +0800

    Read More Button V2.0

--- js/gem-custom.js
+++ js/gem-custom.js
@@ -1,7 +1,7 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+function myFunction_1() {
+  var dots = document.getElementById("dots-1");
+  var moreText = document.getElementById("more-1");
+  var btnText = document.getElementById("myBtn-1");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
@@ -13,3 +13,19 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+function myFunction_2() {
+    var dots = document.getElementById("dots-2");
+    var moreText = document.getElementById("more-2");
+    var btnText = document.getElementById("myBtn-2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  
