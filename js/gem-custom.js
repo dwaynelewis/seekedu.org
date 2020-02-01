69545e10355428bc07eed08b219e561a515b6a51
jbtristan <54360735+jbtristan@users.noreply.github.com>
2020-02-02 00:53:03 +0800

    Read more/less

--- js/gem-custom.js
+++ js/gem-custom.js
@@ -29,3 +29,48 @@ function myFunction_2() {
     }
   }
   
+  function myFunction_community1() {
+    var dots = document.getElementById("dots-community1");
+    var moreText = document.getElementById("more-community1");
+    var btnText = document.getElementById("myBtn-community1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction_community2() {
+    var dots = document.getElementById("dots-community2");
+    var moreText = document.getElementById("more-community2");
+    var btnText = document.getElementById("myBtn-community2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction_community3() {
+    var dots = document.getElementById("dots-community3");
+    var moreText = document.getElementById("more-community3");
+    var btnText = document.getElementById("myBtn-community3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
