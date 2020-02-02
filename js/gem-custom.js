f8e5a949a8f9e124adbf00d8acd5ccd9665f67a3
jbtristan <54360735+jbtristan@users.noreply.github.com>
2020-02-02 10:03:12 +0800

    health page

--- js/gem-custom.js
+++ js/gem-custom.js
@@ -74,3 +74,48 @@ function myFunction_2() {
       moreText.style.display = "inline";
     }
   }
+  function myFunction_health1() {
+    var dots = document.getElementById("dots-health1");
+    var moreText = document.getElementById("more-health1");
+    var btnText = document.getElementById("myBtn-health1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction_health2() {
+    var dots = document.getElementById("dots-health2");
+    var moreText = document.getElementById("more-health2");
+    var btnText = document.getElementById("myBtn-health2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+  function myFunction_health3() {
+    var dots = document.getElementById("dots-health3");
+    var moreText = document.getElementById("more-health3");
+    var btnText = document.getElementById("myBtn-health3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
